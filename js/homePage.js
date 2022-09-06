85eea521b03d801c17a25caf55fc4880e98b984f
Shira Chehanovich <shirachech15@gmail.com>
2022-09-06 18:24:53 +0300

    gjftufyh

--- js/homePage.js
+++ js/homePage.js
@@ -1,3 +1,22 @@
+const modal = document.getElementById(".modal");
+const trigger = document.getElementById(".trigger");
+// const closeButton = document.getElementById(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
 $(function() {
 	setTimeout(function() {
 		$('.fly-in-text').removeClass('hidden');
