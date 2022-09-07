d90e0a8125121f5a34f5f3ae9742d63572c694a1
Rivka Elgozy <rivkaelgozy@gamil.com>
2022-09-07 11:57:54 +0300

    play btn

--- js/homePage.js
+++ js/homePage.js
@@ -1,6 +1,18 @@
 const modal = document.getElementById("modal");
 const trigger = document.getElementById("trigger");
 const closeButton = document.getElementById("close-button");
+const play = document.getElementById("main-play-btn");
+const isNew = document.getElementById("is-new"); 
+const name1 = document.getElementById("name");
+const level = document.getElementById("level");
+
+
+setInterval(()=>{
+    if(level.value != '' && isNew.value != '' && name1.value != ''){
+    play.onclick = function() {
+        play.href="game.html"; 
+      };
+}},10);
 
 function toggleModal() { 
     modal.classList.toggle("show-modal"); 
@@ -22,9 +34,10 @@ trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-
-$(function() {
+(function() {
 	setTimeout(function() {
 		$('.fly-in-text').removeClass('hidden');
 	},500);
 })();
+
+
